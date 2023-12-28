6b071a9aba6fa97245fa834709d4051e6ac9203f
Kinfe123 <kinfishtech@gmail.com>
2023-12-29 00:22:59 +0300

    fix: ui fix

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -26,11 +26,11 @@ export default async function BlogPage() {
       </div>
       <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
         <div className="flex-1 space-y-4">
-          <h1 className="inline-block font-heading text-4xl tracking-tight lg:text-5xl">
-            Blog
+          <h1 className="inline-block fn font-headingAlt bg-gradient-to-tr from-white/50 bg-clip-text text-transparent to-white text-4xl tracking-tight lg:text-5xl">
+            What <span className="mx-[3px]"></span>i <span className="mx-[3px]"></span> thought
           </h1>
-          <p className="text-xl text-muted-foreground">
-            A blog built using Contentlayer. Posts are written in MDX.
+          <p className="text-xl font-sans text-muted-foreground">
+           Homemade thought and farms - all the magic 
           </p>
         </div>
       </div>
