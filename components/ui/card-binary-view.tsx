291d8637569ea1fb3941c9da1ea166e89f733630
Kinfe123 <kinfishtech@gmail.com>
2023-12-28 23:33:56 +0300

    fix: ui fix

--- components/ui/card-binary-view.tsx
+++ components/ui/card-binary-view.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { EvervaultCard, Icon } from "../ui/card-binary";
 import { Separator } from "./separator";
+import Link from "next/link";
 
 type PostCardProps = {
   title: string;
@@ -24,9 +25,33 @@ export function PostCard(props: PostCardProps) {
       {/* <p className="text-sm border font-light dark:border-white/[0.2] border-black/[0.2] rounded-full mt-4 text-black dark:text-white px-2 py-0.5">
         Watch me hover
       </p> */}
-      <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
-        Shimmer
-      </button>
+
+      <Link href={`${props.slug}`}>
+        <button className="bg-transparent mt-2 no-underline group cursor-pointer relative shadow-2xl shadow-zinc-900 rounded-full p-px text-xs font-semibold leading-6  text-white inline-block">
+          <span className="absolute inset-0 overflow-hidden rounded-full">
+            <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100"></span>
+          </span>
+          <div className="relative flex space-x-2 items-center z-10 rounded-full bg-transparent py-1.5 px-4 ring-1 ring-white/10 ">
+            <span>{`Read More`}</span>
+            <svg
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="1.5"
+                d="M10.75 8.75L14.25 12L10.75 15.25"
+              ></path>
+            </svg>
+          </div>
+          <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40"></span>
+        </button>
+      </Link>
     </div>
   );
 }
