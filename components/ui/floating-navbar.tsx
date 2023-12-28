8eed399fff3d43fdc5449017cbdb89a4a7cd954f
Kinfe123 <kinfishtech@gmail.com>
2023-12-28 15:12:16 +0300

    fix: quick ui fix

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -51,7 +51,7 @@ import Link from "next/link";
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-gradient-to-tr dark:from-purple-900/90 dark:to-black  bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4",
+          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border border-transparent dark:border-white/[0.2] rounded-full dark:bg-gradient-to-tr dark:from-purple-900 dark:to-black  bg-white shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] pr-2 pl-8 py-2  items-center justify-center space-x-4",
           className,
         )}
       >
