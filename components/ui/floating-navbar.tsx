73dd5666b20a03fcc50787ca1053014fe7c38103
Kinfe123 <kinfishtech@gmail.com>
2024-01-31 15:20:50 +0300

    feat: new icon

--- components/ui/floating-navbar.tsx
+++ components/ui/floating-navbar.tsx
@@ -69,7 +69,7 @@ const FloatingNav = ({
                 )}
               >
                 {/* <span className="block sm:hidden">{navItem.icon}</span> */}
-                <span className="hidden text-sm sm:block mr-5">{navItem.name}</span>  <Icon className=" h-6 w-6 bg-transparent transform rotate-180 mx-20 " />
+                <span className="hidden text-sm sm:block mr-[1.20rem]">{navItem.name}</span>  <Icon className=" h-6 w-6 bg-transparent text-gray-300/40 transform rotate-45 mx-20 " />
               </Link>
             ))}
             <button className="relative py-2 px-4 text-sm font-medium text-black rounded-full border dark:text-white border-neutral-200 dark:border-white/[0.2]">
