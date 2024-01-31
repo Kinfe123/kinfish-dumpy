eaf1f96aa10925355fdce925c8260f8f799b231c
Kinfe123 <kinfishtech@gmail.com>
2024-01-31 16:03:37 +0300

    adding schema col

--- db/schema.ts
+++ db/schema.ts
@@ -3,24 +3,19 @@ import { serial, text, timestamp, pgTable } from "drizzle-orm/pg-core";
 export var visitor = pgTable("visitor", {
   id: serial("id"),
   name: text("name"),
-  
 });
 export var guesser = pgTable("guesser", {
   id: serial("id"),
   name: text("name"),
-  phone: text('phone'),
-  guess: text('guess')
-
-  
+  phone: text("phone"),
+  guess: text("guess"),
 });
-
 
 export var subscribers = pgTable("subscribers", {
   id: serial("id"),
   name: text("name"),
-  email: text('email')
+  email: text("email"),
+  createdAt: timestamp("created_at").defaultNow().notNull(),
+
 
 });
-
-
-
