db08cbb714d2eb9e40a48ea1ae5c763cc5e507f2
Kinfe123 <kinfishtech@gmail.com>
2024-02-25 11:10:06 +0300

    fix: hover effect

--- components/ui/card-binary-view.tsx
+++ components/ui/card-binary-view.tsx
@@ -13,7 +13,7 @@ type PostCardProps = {
 };
 export function PostCard(props: PostCardProps) {
   return (
-    <div className="border border-black/[0.2] bg-gradient-to-tr from-purple-400/10 to-transparent dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[25rem] w-[25rem]">
+    <div className="border border-black/[0.2] bg-gradient-to-tr from-purple-400/10 to-transparent/5 dark:border-white/[0.2] flex flex-col items-start max-w-sm mx-auto p-4 relative h-[25rem] w-[25rem]">
       <Icon className="absolute h-6 w-6 -top-3 -left-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -bottom-3 -left-3 dark:text-white text-black" />
       <Icon className="absolute h-6 w-6 -top-3 -right-3 dark:text-white text-black" />
