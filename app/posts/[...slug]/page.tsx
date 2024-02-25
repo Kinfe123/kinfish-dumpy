e0a59d04711db28b3deb0720388448bf2a61323e
Kinfe123 <kinfishtech@gmail.com>
2024-02-25 11:20:56 +0300

    fix: font and case fix

--- app/posts/[...slug]/page.tsx
+++ app/posts/[...slug]/page.tsx
@@ -125,7 +125,7 @@ export default async function PostPage({ params }: PostProps) {
               <span className="mx-1"></span> {` `} {post.readTime} {` min`}
             </time>
           )}
-          <h1 className="flex mt-2 text-6xl sm:text-7xl max-w-5xl  text-right  md:text-8xl lg:text-9xl leading-tight ml-auto  dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-tr font-headingAlt dark:from-zinc-400/10 dark:via-white/90 dark:to-white/10">
+          <h1 className="flex mt-2 text-6xl sm:text-7xl max-w-5xl font-heading  text-right  md:text-8xl lg:text-9xl leading-tight ml-auto  dark:text-transparent dark:bg-clip-text dark:bg-gradient-to-tr  dark:from-zinc-400/10 dark:via-white/90 dark:to-white/10">
             {post.title}
           </h1>
           <p className="text-muted-foreground max-w-5xl mr-1 text-right text-xl md:text-xl">{post.description}</p>
